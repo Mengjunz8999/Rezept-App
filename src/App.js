380c16d2dc67cd68a158814379a6725d619d91a4
pasichnykTetiana-create <pasichnyktetiana.de@gmail.com>
2025-12-02 11:42:42 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import "./App.css";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import MealPage from "./pages/MealPages/MealPage";
+
+import "@fontsource/happy-monkey";
 
 function App() {
-  return <div className="App">Test Branch</div>;
+  return <div className="App">Test Mengjun</div>;
 }
 
 export default App;
+git commit -m "Merge branch 'tetiana' into main"
+
